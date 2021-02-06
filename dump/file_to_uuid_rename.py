a1d9e782b0cb641cc20271579b1d5ea3f037376e
raj3shp <44482451+raj3shp@users.noreply.github.com>
2021-02-06 15:41:31 +0100

    Update file_to_uuid_rename.py

--- dump/file_to_uuid_rename.py
+++ dump/file_to_uuid_rename.py
@@ -2,17 +2,27 @@ import os
 import uuid
 from shutil import copyfile
 
-# Enter property IDs here
-property_ids = [1, 2]
+ids = input("Enter property IDs in comma separated (ex. 1, 2, 3):")
+
+for id in ids.split(","):
+    property_ids.append(int(id))
 
 if not os.path.exists('./images'):
     os.makedirs('./images')
 
+if os.path.exists("output.csv"):
+    os.remove("output.csv")
+
+out_file = open("output.csv", "w")
+out_file.write("property_id,uuid_filename\n")
+
 for id in property_ids:
   for filename in os.listdir("./{}".format(id)):
+    if filename.startswith("."):
+        continue
     file_format = filename.split(".")[1]
     uuid_name = "{}.{}".format(uuid.uuid4(), file_format)
     file_name_path = "./{}/{}".format(id, filename)
     # os.rename(file_name_path, uuid_name)
     copyfile(file_name_path, "./images/{}".format(uuid_name))
-    print("{},{}".format(id, uuid_name))
+    out_file.write("{},{}\n".format(id, uuid_name))
